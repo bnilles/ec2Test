1ada714b68ca57eb418b771950709e648510fac5
bnilles <joseph.nilles@substack.com>
2021-08-17 21:26:57 -0700

    new update

--- ec2Test/Startup.cs
+++ ec2Test/Startup.cs
@@ -31,12 +31,12 @@ namespace ec2Test
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            /*if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-            }
+            }*/
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
